9795fcd99b5ff064b6c855d3ad932d3d35bd983c
Patrick Koss <49844980+PatrickKoss@users.noreply.github.com>
2023-09-03 11:36:07 +0200

    Update StudentUpdateRequest.java

--- java/rest-simple-exercise/src/main/java/com/example/restsimple/request/StudentUpdateRequest.java
+++ java/rest-simple-exercise/src/main/java/com/example/restsimple/request/StudentUpdateRequest.java
@@ -3,8 +3,4 @@ package com.example.restsimple.request;
 public class StudentUpdateRequest extends StudentCreateRequest {
     public StudentUpdateRequest() {
     }
-
-    public StudentUpdateRequest(String name, String lastName) {
-        super(name, lastName);
-    }
 }
