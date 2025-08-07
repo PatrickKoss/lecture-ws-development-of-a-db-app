57ef8abe21aad7853af5fe7b0aa79b6d6a80c3a2
Patrick Koss <patrick.koss@stackit.cloud>
2025-08-07 21:56:34 +0200

    fix mac setup

--- java/rest-simple/settings.gradle.kts
+++ java/rest-simple/settings.gradle.kts
@@ -1,2 +1,20 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+        maven("https://repo.spring.io/release")
+    }
+    resolutionStrategy {
+        eachPlugin {
+            if (requested.id.id == "org.springframework.boot") {
+                useModule("org.springframework.boot:spring-boot-gradle-plugin:${requested.version}")
+            }
+            if (requested.id.id == "io.spring.dependency-management") {
+                useModule("io.spring.gradle:dependency-management-plugin:${requested.version}")
+            }
+        }
+    }
+}
+
 rootProject.name = "rest-simple"
 
